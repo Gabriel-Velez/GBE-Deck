88df8c44b79f5a1babd9cff031f4631badb8f21b
gabriel velez <gabriel.dan.velez@gmail.com>
2025-06-26 13:47:37 -0400

    changed base to match custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/GBE-Deck/",
+  base: "./",
   server: {
     host: true, // or use "0.0.0.0"
   },
